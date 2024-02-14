6920e45f02a9d436c9f8d2bef37c3f0493130014
Gleb Lukin <uniteforuprising@gmail.com>
2024-02-14 14:49:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b, c;
+    c = a + b;
+    cout << "a ="; 
+    cin>> a;
+    cout << "b="; 
+    cin>> b;
+    cout << c;
+}
+
